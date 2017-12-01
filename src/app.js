e9a084085c02d63b4fa362c552998e3d617a4142
Ashutosh Singh Rawat <singh.ashutoshrawat@gmail.com>
2017-12-02 02:14:48 +0530

    React redux integration

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 "use strict"
 
+import React from 'react';
+import {render} from 'react-dom';
+ 
 import {applyMiddleware,createStore} from 'redux';
 import logger from 'redux-logger';
 
@@ -9,50 +12,8 @@ import reducers from './reducers'
 //IMPORT ACTIONS
 import {addToCart} from './actions/cartActions';
 import {postBooks, deleteBooks, updateBooks} from './actions/booksActions';
+
 //STEP 3 define reducers
-// const reducer = function(state={books: []}, action){
-// 	switch(action.type){
-// 		// case 'INCREMENT':
-// 		// 	return state + action.payload;
-// 		// 	break;
-// 		// case 'DECREMENT':
-// 		// 	return state - action.payload;
-// 		// 	break;
-// 		case 'POST_BOOK':
-// 			// return state = action.payload;
-// 			// let books = state.books.concat(action.payload);
-// 			// return {books};
-// 			return {books: [...state.books, ...action.payload]};
-// 			break;
-
-// 		case 'DELETE_BOOK':
-// 			const currentBookToDelete = [...state.books]
-// 			const indexToDelete = currentBookToDelete.findIndex(function(book){
-// 				return book.id === action.payload.id;
-// 			})
-// 			return {books: [...currentBookToDelete.slice(0, indexToDelete), ...currentBookToDelete.slice(indexToDelete+1)]};
-// 			break;
-
-// 		case 'UPDATE_BOOK':
-// 			const currentBookToUpdate = [...state.books]
-// 			const indexToUpdate = currentBookToUpdate.findIndex(function(book){
-// 				return book.id === action.payload.id;
-// 			})
-
-// 			const newBookToUpdate = {
-// 				...currentBookToUpdate[indexToUpdate],
-// 				title: action.payload.title
-// 			}
-
-// 			console.log("------- newBookToUpdate ==== ", newBookToUpdate);
-
-// 			return {books: [...currentBookToUpdate.slice(0, indexToUpdate), newBookToUpdate, ...currentBookToUpdate.slice(indexToUpdate+1)]};
-// 			break;
-
-
-// 	}
-// 	return state
-// }
 
 //STEP 1 create the store
 const middleware = applyMiddleware(logger);
@@ -62,6 +23,14 @@ const store = createStore(reducers, middleware);
 // 	console.log('current stage is: ', store.getState());
 // 	// console.log('current price: ', store.getState()[1].price);
 // })
+
+//IMPORT 
+import BookList from './components/pages/booksList';
+
+
+render(
+	<BookList/>, document.getElementById('app-root')
+);
 
 //STEP 2 create and dispatch actions
 // store.dispatch({type: 'INCREMENT', payload: 1})
