a3ea557e4bb5152dc8fefb24a6da0bd956de5718
Ashutosh Singh Rawat <singh.ashutoshrawat@gmail.com>
2017-12-03 12:58:03 +0530

    Checout Modal

--- src/components/pages/cart.js
+++ src/components/pages/cart.js
@@ -1,7 +1,7 @@
 "use strict"
 import React from 'react';
 import {connect} from 'react-redux';
-import {Panel, Row, Col, Well, Button, ButtonGroup, Label} from 'react-bootstrap';
+import {Panel, Row, Col, Well, Button, ButtonGroup, Label, Modal} from 'react-bootstrap';
 
 import {bindActionCreators} from 'redux';
 
@@ -25,6 +25,19 @@ class Cart extends React.Component{
 		if(quantity > 1){
 			this.props.updateCart(_id, -1)
 		}
+	}
+
+	constructor(){
+		super();
+		this.state = {
+			showModal: false
+		}
+	}
+	open(){
+		this.setState({showModal: true})
+	}
+	close(){
+		this.setState({showModal: false})
 	}
 
 	render(){
@@ -72,6 +85,29 @@ class Cart extends React.Component{
 		return(
 			<Panel header='Cart' bsStyle='primary'>
 				{cartItemList}
+				<Row >
+					<Col xs={12}>
+						<h6>Total Amount: </h6>
+						<Button onClick={this.open.bind(this)} bsStyle='success' bsSize='small'>
+							PROCEED TO CHECKOUT
+						</Button>
+					</Col>
+				</Row>
+				<Modal show={this.state.showModal} onHide={this.close.bind(this)}>
+		          	<Modal.Header closeButton>
+		            	<Modal.Title>Thank you!</Modal.Title>
+		          	</Modal.Header>
+		          	<Modal.Body>
+		          		<h6>Your Order has been saved</h6>
+		          		<p>You will receive an email confirmation</p>
+		          	</Modal.Body>
+		          	<Modal.Footer>
+		          		<Col xs={6}>
+		          			<h6>Total $: </h6>
+		          		</Col>
+		            	<Button onClick={this.close.bind(this)}>Close</Button>
+		          	</Modal.Footer>
+		        </Modal>
 			</Panel>
 		)
 	}
