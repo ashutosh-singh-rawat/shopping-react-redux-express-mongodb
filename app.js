7299611ca06a1ef89f5514be598ec4a7223a30c6
Ashutosh Singh Rawat <singh.ashutoshrawat@gmail.com>
2017-12-08 13:53:10 +0530

    Server Side Rendering

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require('babel-core/register')({
+	"presets": ["es2015", "react", "stage-1"]
+});
 var express = require('express');
-var path = require('path');
+var path 		= require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var logger 	= require('morgan');
 
 //PROXY
 var httpProxy = require('http-proxy');
@@ -23,21 +26,27 @@ app.use('/api', function (req, res) {
 });
 //END PROXY
 
+// uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', index);
-// app.use('/users', users);
-app.get('*', function(req, res){
-	res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-});
+// REQUEST HANDLER FOR SERVER-SIDE RENDERING
+var requestHandler = require('./requestHandler.js');
+app.set('view engine', 'ejs');
+app.use(requestHandler);
+
+
+// // app.use('/', index);
+// // app.use('/users', users);
+// app.get('*', function(req, res){
+// 	res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
